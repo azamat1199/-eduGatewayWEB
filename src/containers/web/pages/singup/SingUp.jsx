eeebf025dfe9dec519ca7a60d7f539edb3358dd9
Abdulloh <abdullohjonirgashev@gmail.com>
2021-09-13 14:42:15 +0500

    authoritaion

--- src/containers/web/pages/singup/SingUp.jsx
+++ src/containers/web/pages/singup/SingUp.jsx
@@ -1,28 +1,29 @@
-import React, { Component, useState, useCallback,useRef } from "react";
+import React, { Component, useState, useCallback, useRef } from "react";
 import axios from "axios";
-import { NavLink,useHistory } from "react-router-dom";
+import { NavLink, useHistory } from "react-router-dom";
 import google from "../../../../assets/icon/google.svg";
 import facebook from "../../../../assets/icon/facebookreg.svg";
 import view from "../../../../assets/icon/view.svg";
-import check from "../../../../assets/icon/check.svg";
+import check from "../../../../assets/icon/checked.svg";
 import "../../../../style/css/singup.css";
 import Navbar from "../Navbar";
-import InputErrorMsg from './inputErrorMsg'
+import InputErrorMsg from "./inputErrorMsg";
 import Axios from "../../../../utils/axios";
-import { Progress } from 'react-sweet-progress';
+import { Progress } from "react-sweet-progress";
 import "react-sweet-progress/lib/style.css";
-import Loader from 'react-loader'
-
-
+import Loader from "react-js-loader";
+import { Spin, message } from "antd";
 
 function SingUp() {
-  const history = useHistory()
+  const history = useHistory();
+  const inputRef = useRef();
+  const buttonRef = useRef();
+  const statsuRef = useRef();
+  const [loading, setLoading] = useState(false);
   const [type, setType] = useState(true);
-  const student = 'student';
-  const [error,setError] = useState({type:'',message:''})
-  console.log(error);
-  const inputRef = useRef()
-  console.log(inputRef);
+  const [error, setError] = useState("");
+  const [length, setLength] = useState();
+  const [status, setStatus] = useState("");
   const [loginData, setLoginData] = useState({
     first_name: "",
     last_name: "",
@@ -30,71 +31,67 @@ function SingUp() {
     password_2: "",
     phone_number: "",
   });
-const [length,setLength] = useState()
-const [status,setStatus] = useState('')
+
   const handleInputChange = useCallback(
     (e) => {
       const { name, value } = e.target;
       setLoginData((state) => ({ ...state, [name]: value }));
-	  if (name === "password_1" && !value.length ) {
-        setStatus("error")
-	    setLength(0)
-	  }
-	  else if (name === "password_1" && value.length < 2) {
-        setStatus("error")
-	    setLength(12.5)
-	  }
-	  else if (name === "password_1" && value.length < 3) {
 
-        setStatus("error")
-	    setLength(25)
-	  }
-	  else if (name === "password_1" && value.length < 4) {
-        setStatus("error")
-	    setLength(37.5)
-	  }
-	  else if (name === "password_1" && value.length < 5) {
-        setStatus("error")
-	    setLength(50)
-	  }
-	  else if (name === "password_1" && value.length < 6) {
-
-        setStatus("error")
-	    setLength(62.5)
-	  }
-	  else if (name === "password_1" && value.length < 8) {
-		const{current} = inputRef
-		current.style = "background:red"
-        setStatus("error")
-	    setLength(75)
-	  }
-	 
-	  else {
-		setLength(100)
-		setStatus("success")
-	  }
+      if (name === "password_1" && !value.length) {
+        setStatus("error");
+        setLength(0);
+      } else if (name === "password_1" && value.length < 2) {
+        setStatus("error");
+        setLength(12.5);
+      } else if (name === "password_1" && value.length < 3) {
+        setStatus("error");
+        setLength(25);
+      } else if (name === "password_1" && value.length < 4) {
+        setStatus("error");
+        setLength(37.5);
+      } else if (name === "password_1" && value.length < 5) {
+        setStatus("error");
+        setLength(50);
+      } else if (name === "password_1" && value.length < 6) {
+        setStatus("error");
+        setLength(62.5);
+      } else if (name === "password_1" && value.length < 8) {
+        const { current } = inputRef;
+        current.style = "background:red";
+        setStatus("error");
+        setLength(75);
+      } else if (name === "password_1" && value.length == 8) {
+        setStatus("success");
+        setLength(100);
+      }
     },
     [loginData]
   );
-  console.log(loginData);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setLoading(true);
     try {
       const res = await Axios.post("/common/auth/register", loginData);
-	  const {status} = res
-	  const {data} = res
-	  if(status == 201) {
-		 history.push('/requisition')
-	  }
+      const { status } = res;
+      const { data } = res;
+      if (status == 201) {
+        history.push("/requisition");
+      }
       console.log(data);
+      setLoading(false);
     } catch (err) {
-		const {error} = err.response.data
-		console.log(error[0]);
-	  
+      const { error } = err.response?.data;
+      console.log(error[0]?.error[0]);
+      setError(error[0]?.error[0]);
+      setLoading(false);
     }
   };
-
+  
+  console.log(error);
+  console.log(inputRef);
+  console.log(status);
+  console.log(loginData);
   return (
     <React.Fragment>
       <div className="navRegist">
@@ -163,13 +160,23 @@ const [status,setStatus] = useState('')
           <h1>Регистрация</h1>
           <div className="form_div">
             <p>Ваша имя </p>
-            <input onChange={handleInputChange} type="text" name="first_name" />
-			<InputErrorMsg type="first_name" errorObj={error} />
+            <input
+              onChange={handleInputChange}
+              type="text"
+              name="first_name"
+              required
+            />
+            <InputErrorMsg type="first_name" errorObj={error} />
           </div>
           <div className="form_div">
             <p>Ваша фамилия</p>
-            <input onChange={handleInputChange} type="text" name="last_name" />
-			<InputErrorMsg type="last_name" errorObj={error} />
+            <input
+              onChange={handleInputChange}
+              type="text"
+              name="last_name"
+              required
+            />
+            <InputErrorMsg type="last_name" errorObj={error} />
           </div>
           <div className="form_div">
             <p>Ваш телефон номера</p>
@@ -177,39 +184,97 @@ const [status,setStatus] = useState('')
               type="phone"
               onChange={handleInputChange}
               name="phone_number"
+              required
             />
           </div>
           <div className="form_div">
             <p>Выберите пароль</p>
+
             <div className="password">
               <input
                 onChange={handleInputChange}
                 name="password_1"
-				ref={inputRef}
+                required
+                ref={inputRef}
                 type={type ? "password" : "text"}
               />
               <img onClick={() => setType(() => !type)} src={view} alt="" />
             </div>
-			<div style={{height:'2px',width:""}}></div>
-			 {/* <p style={{color:'red'}}>{length}</p> */}
-			<div className="status-bar">
-		    	<Progress ref={inputRef} percent={length} status={status} />
-				{loginData.password_1.length < 8 ? <div className="statusPercent"><div> <span style={{color:'red'}}>{loginData.password_1.length}/</span><span style={{fontSize:'20px',fontWeight:'500',color:'red'}}>8</span> </div></div>: null}
-		        
-			</div>
+            <div
+              style={
+                loginData.password_1.length > 0
+                  ? { display: "flex" }
+                  : { display: "none" }
+              }
+              ref={statsuRef}
+              className="status-bar"
+            >
+              <Progress ref={inputRef} percent={length} status={status} />
+              {loginData.password_1.length < 8 ? (
+                <div style={{ marginLeft: "20px" }} className="statusPercent">
+                  <div>
+                    {" "}
+                    <span style={{ color: "red" }}>
+                      {loginData.password_1.length}/
+                    </span>
+                    <span
+                      style={{
+                        fontSize: "20px",
+                        fontWeight: "500",
+                        color: "red",
+                      }}
+                    >
+                      8
+                    </span>{" "}
+                  </div>
+                </div>
+              ) : null}
+            </div>
           </div>
           <div className="form_div">
             <p>Повторите пароль</p>
             <div className="password">
               <input
+                onClick={() =>
+                  loginData.password_1.length >= 8 > 0
+                    ? (statsuRef.current.style = "display:none;")
+                    : ""
+                }
                 onChange={handleInputChange}
                 type="password"
                 name="password_2"
+                required
               />
-              {loginData.password_1 == loginData.password_2 ? <img src={check} alt="" />:<div class="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>}
+              {loginData.password_1 == loginData.password_2 &&
+              loginData.password_2 != "" ? (
+                <img src={check} alt="" />
+              ) : loginData.password_2.length > 0 ? (
+                <Loader type="box-up" bgColor={"black"} size={40} />
+              ) : (
+                ""
+              )}
             </div>
           </div>
-          <button className="reg_btn">Зарегистрироваться</button>
+
+          <p style={{ color: "red", marginBottom: "8px", fontWeight: "600" }}>
+            {" "}
+            {error}
+          </p>
+          <button
+            ref={buttonRef}
+            style={
+              loading ? { background: "#8cb4c5" } : { background: "#00587F" }
+            }
+            className="reg_btn"
+          >
+            {loading ? (
+              <>
+                <Spin size="middle" spinning={loading} />
+              </>
+            ) : (
+              "Зарегистрироваться"
+            )}
+          </button>
           <h2>или</h2>
           <h2>Войдите через</h2>
           <a className="reg_link" href="#">
